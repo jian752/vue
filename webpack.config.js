6217975f21285e13054457c136186a3db5063f85
Will Wu <will.wu@latticeworkinc.com>
2021-04-07 15:30:09 +0800

    Successfully build index.html, favicon.ico and main.build.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
     entry: './src/main.js', // entry point
@@ -6,5 +7,12 @@ const config = {
         path: path.resolve(__dirname + '/dist'), // package destination, need absolute path
         filename: '[name].build.js', // package name
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico',
+        }),
+    ],
 };
 module.exports = config;
