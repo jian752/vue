7a4c54f1215b18958afb0bb3376d2a1af671149e
Will Wu <will.wu@latticeworkinc.com>
2021-04-07 15:36:58 +0800

    import vue loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 const config = {
     entry: './src/main.js', // entry point
@@ -7,12 +8,28 @@ const config = {
         path: path.resolve(__dirname + '/dist'), // package destination, need absolute path
         filename: '[name].build.js', // package name
     },
+    // loaders
+    module: {
+        rules: [
+            {
+                // *.vue
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                // `<style>` and `*.css` of `*.vue`
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader'],
+            },
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'public/index.html',
             favicon: 'public/favicon.ico',
         }),
+        new VueLoaderPlugin(),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
